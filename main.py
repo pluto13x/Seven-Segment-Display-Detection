58b80f4e7225c59de7b1e5cbd3b9043318b4552d
pluto13x <saramilosavljevic6@gmail.com>
2026-05-08 22:10:55 +0200

    rotate image and make canvas bigger to fit rotated image

--- main.py
+++ main.py
@@ -34,22 +34,44 @@ for filename in os.listdir(folder): #load images
 
     large_contours = []
 
-    for contour in contours:
+    for contour in contours: #find contours
         area = cv2.contourArea(contour)
 
         if area > 100:
             large_contours.append(contour)
 
-    output = cv2.cvtColor(led_img, cv2.COLOR_GRAY2BGR)
+    all_points = np.vstack(large_contours) #combine contours into one array
 
-    cv2.drawContours(
-        output,
-        large_contours,
-        -1,
-        (0, 255, 0),
-        3
+    rect = cv2.minAreaRect(all_points) #find rotated bounding box - finds smallest possible bounding box
+
+    (center_x, center_y), (width, height), angle = rect #find angle of bounding box
+
+    if width < height: #normalize angle
+        angle = angle + 90
+
+    print("fixed angle:", angle)
+
+    height_img, width_img = img.shape[:2]
+
+    center = (width_img // 2, height_img // 2)
+
+    rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+
+    cos = abs(rotation_matrix[0, 0])
+    sin = abs(rotation_matrix[0, 1])
+
+    new_width = int((height_img * sin) + (width_img * cos))
+    new_height = int((height_img * cos) + (width_img * sin))
+
+    rotation_matrix[0, 2] += (new_width / 2) - center[0]
+    rotation_matrix[1, 2] += (new_height / 2) - center[1]
+
+    rotated = cv2.warpAffine(
+        img,
+        rotation_matrix,
+        (new_width, new_height)
     )
 
-    plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
+    plt.imshow(cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB))
     plt.axis("off")
     plt.show()
